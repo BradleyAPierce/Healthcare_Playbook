d6c7a8d0412c26fb3a075f86535555614acc63b3
BradleyAPierce <bradleyapierce@me.com>
2024-05-12 12:26:21 -0700

    Commit

--- popupcontentfolder/popupcontent.js
+++ popupcontentfolder/popupcontent.js
@@ -2,8 +2,8 @@ window.onload = function() {
     var myModal = new mdb.Modal(document.getElementById('pageenterModal'));
   
     // Define the start and end dates for the modal display
-    var startDate = new Date('May 11, 2024 09:00:00');
-    var endDate = new Date('June 15, 2024 17:00:00');
+    var startDate = new Date('May 10, 2024 09:00:00');
+    var endDate = new Date('May 10, 2024 17:00:00');
     var currentDate = new Date();
   
     // Check if the current date is within the range
