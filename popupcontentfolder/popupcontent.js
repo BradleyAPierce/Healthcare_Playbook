04e3e4cf3df8c1adbd4a200a24f6d177ce434ab2
BradleyAPierce <bradleyapierce@me.com>
2025-09-28 23:03:00 -0700

    fix(popupcontent): update modal end date to December 31, 2025 for accurate display range

--- popupcontentfolder/popupcontent.js
+++ popupcontentfolder/popupcontent.js
@@ -1,23 +1,24 @@
-window.onload = function() {
-    var myModal = new mdb.Modal(document.getElementById('pageenterModal'));
-  
-    // Define the start and end dates for the modal display
-    var startDate = new Date('April 20, 2025 09:00:00');
-    var endDate = new Date('July 31, 2025 17:00:00');
-    var currentDate = new Date();
-  
-    // Check if the current date is within the range
-    if (currentDate >= startDate && currentDate <= endDate) {
-      myModal.show();
-    }
-  };
-  
-  document.addEventListener('DOMContentLoaded', function() {
-    fetch('https://bradleyapierce.github.io/Healthcare_Playbook/popupcontentfolder/popupheader.txt')
-        .then(response => response.text())
-        .then(text => {
-            document.getElementById('popupModalLabel').textContent = text;
-        })
-        .catch(error => console.error('Error loading the text:', error));
-});
+window.onload = function () {
+  var myModal = new mdb.Modal(document.getElementById("pageenterModal"));
 
+  // Define the start and end dates for the modal display
+  var startDate = new Date("April 20, 2025 09:00:00");
+  var endDate = new Date("December 31, 2025 17:00:00");
+  var currentDate = new Date();
+
+  // Check if the current date is within the range
+  if (currentDate >= startDate && currentDate <= endDate) {
+    myModal.show();
+  }
+};
+
+document.addEventListener("DOMContentLoaded", function () {
+  fetch(
+    "https://bradleyapierce.github.io/Healthcare_Playbook/popupcontentfolder/popupheader.txt"
+  )
+    .then((response) => response.text())
+    .then((text) => {
+      document.getElementById("popupModalLabel").textContent = text;
+    })
+    .catch((error) => console.error("Error loading the text:", error));
+});
